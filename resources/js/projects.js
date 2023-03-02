35a9cd870cfd967eee6f0706a054dc3635812652
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2023-03-01 16:05:54 -0800

    Add files via upload

---
+++ resources/js/projects.js
@@ -0,0 +1,6 @@
+var menu = document.querySelector(".menu");
+var burger = document.querySelector(".burger");
+
+burger.addEventListener("click", function() {
+    menu.classList.toggle("shown");
+});
