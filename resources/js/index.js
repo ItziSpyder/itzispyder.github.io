d0fc133795352f8c7c3b0a0cfe24a820a37c4745
ImproperIssues <114215797+ItziSpyder@users.noreply.github.com>
2023-04-03 11:19:16 -0700

    Delete resources directory

--- resources/js/index.js
+++
@@ -1,6 +0,0 @@
-var menu = document.querySelector(".menu");
-var burger = document.querySelector(".burger");
-
-burger.addEventListener("click", function() {
-    menu.classList.toggle("shown");
-});
